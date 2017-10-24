c523c682b4077b383461f194f93cc7a2bae1c478
Mauderica <maude.chalin@gmail.com>
2017-10-24 11:31:37 -0400

    First submit to Udacity

---
+++ TODO.md
@@ -0,0 +1,47 @@
+# Pixel Art Maker Project
+
+## To-Do Later
+
+* Get the screen size with jQuery and change my code to apply different grid-size-maximum conditionals based on the user's screen size:
+https://stackoverflow.com/questions/3437786/get-the-size-of-the-screen-current-web-page-and-browser-window
+* Improve the aesthetics and layout of the `<form>`.
+* Make the webpage responsive such that layout, text size, and grid cell size change depending on the user's screen size for optimal UX.
+* Fix color scheme.
+* Add Front-End Fairy logo
+
+## To-Do List
+
+* ...
+
+## Questions
+
+* ...
+
+## Done!
+
+* Download or host jQuery (minified) & include in my HTML. -- DONE
+* programmatically ensure that user cannot input grid dimensions larger than a maximum X (decide) -- DONE
+* check if `<input>` tag can handle it, if not --> programmatically ensure that user cannot input grid dimensions less than a minimum 1 -- DONE
+* add a feature that somehow highlights a cell when it is hovered over -- DONE
+* add functionality that allows user to undo the coloring of a cell by clicking on it again, AND adding a new color over an old one? -- DONE
+* Should designs.js really be implemented in the `<body>` tag? What about using some JavaScript '.ready' functionalities? -- Done: document ready handler registered by passing my function into jQuery.
+* Should the script be moved to the `<head>`? What is the convention? -- Answer: It depends, but typically stylesheets should be included in the `<head>` and and scripts at the bottom before the closing `</body>` tag.
+* provide a "clear canvas" button -- DONE
+* add a placeholder of some sort to the design canvas section that initializes before any user input? (initialize it as a 10x10 grid --> add some colored cells) -- DONE
+* add a message that informs the user their art will be erased if they re-submit a grid size (only if there is non-white art...) -- DONE
+* fix conditional per diagram -- DONE
+* mobile-friendly responsiveness -- TDL (to-do later)
+    * add footer with contact info -- DONE
+    * add clear sections (header, body, sub-body etc) -- DONE
+    * better the layout -- DONE (partial)
+* personal style (at least remove random pixels and draw a recognizable image instead - that or no color at all) -- DONE
+* verify I used the correct keyword to declare variables -- DONE
+* document lessons learned -- DONE
+* single quotes, in JS file -- DONE
+* factor out functions - how much? -- DONE
+* format doc -- DONE
+* clean up, organize & document code -- DONE
+* check design against project criteria, grading rubric & Udcity Style Guides -- DONE
+
+
+
--- designs.js
+++ designs.js
@@ -1,10 +1,90 @@
-// Select color input
-// Select size input
+// REGISTER DOCUMENT READY HANDLER
+$(function () {
 
-// When size is submitted by the user, call makeGrid()
+    // FUNCTION DEFINITION: Create a grid using HTML table elements
+    function makeGrid(height, width) {
+        console.log(`>> makeGrid(${height}, ${width})`);
+        const table = $('#pixel_canvas');
+        // Clear the previous table before creating a new one
+        table.empty();
+        // Create new grid
+        for (let r = 1; r <= height; r++) {
+            const row = $('<tr></tr>');
+            table.append(row);
+            for (let c = 1; c <= width; c++) {
+                const cell = $('<td></td>');
+                row.append(cell);
+            }
+        }
+    }
 
-function makeGrid() {
+    // FUNCTION DEFINITION: Call makeGrid() or send alert
+    function checkGrid() {
+        // Get the grid dimensions submitted by the user:
+        const gridHeight = $('input[type=number][name=height]').val();
+        const gridWidth = $('input[type=number][name=width]').val();
+        // Check the submitted dimensions against max and min dimensions allowed:
+        if ((gridHeight > 0 && gridWidth > 0) && (gridHeight < 50 && gridWidth < 50)) {
+            console.log('Grid dimension limits respected.');
+            makeGrid(gridHeight, gridWidth);
+        } else {
+            console.log('Grid dimension limits not respected.');
+            alert('No can do! :) Please input an integer greater than 0 and less than 50.');
+        }
+    }
 
-// Your code goes here!
+    // EVENT LISTENER: Upon the user submitting grid size, check if anything was drawn and call checkGrid()
+    $('input[type=submit]').click(function (event) {
+        // Prevent form submission
+        event.preventDefault(); // document where got this from
+        // Check whether the user has drawn anything:
+        // (https://stackoverflow.com/questions/10641258/jquery-select-data-attributes-that-arent-empty)
+        const colorCheck = $('td[style][style!=""]').length;
+        console.log(`Has the user drawn something: ${colorCheck > 0}. This many cells were colored: ${colorCheck}`);
+        // If the user has drawn something, ask for confirmation of grid change
+        if (colorCheck === 0) {
+            checkGrid();
+        } else if (confirm('Careful! Submitting a grid size will also clear your art. Please confirm submission.')) {
+            console.log('The user has confirmed the grid change');
+            checkGrid();
+        } else {
+            console.log('The user has cancelled the grid change.');
+        }
+        // Prevent form submission
+        return false;
+    });
 
-}
+    // EVENT LISTENER: Log the color picked by the user
+    $('#colorPicker').on('input', function () {
+        console.log(`The user has selected the color ${$('#colorPicker').val()}`);
+    });
+
+    // EVENT LISTENER: Upon the user clicking a grid cell, change the cell background color appropriately
+    // My HEX-to-RGB color converter <div>
+    const colorConverter = $('<div></div>');
+    // Event Listener:
+    $('#pixel_canvas').on('click', 'td', function () {
+        // Get the value of the color picked by the user
+        const colorPicked = $('#colorPicker').val();
+        console.log(`>> The current color picked in HEX is ${colorPicked}`); // analytics code
+        // Convert the color picked from HEX to RGB format by assigning it to a non-attached <div> element
+        const colorPickedRGB = colorConverter.css('background-color', colorPicked).css('background-color');
+        console.log(`>> The current color picked in RGB is ${colorPickedRGB}`); // analytics code
+        // Get the initial background-color value of the clicked cell
+        const colorStart = $(this).css('background-color');
+        // Set the clicked cell's color back to white, or to a new color depending on:
+        if (colorStart === colorPickedRGB) {
+            $(this).removeAttr('style');
+            console.log(`>> This cell ${JSON.stringify(this)} was colored ${colorStart}. It is now blank canvas.`); // analytics code
+        } else {
+            $(this).css('background-color', colorPicked);
+            console.log(`>> This cell was colored ${colorStart}. It is now ${colorPicked}.`); // analytics code
+        }
+    });
+
+    // EVENT LISTENER: Upon the user clicking the 'clean canvas' button --> clear grid of any colours but keep at current user-selected size
+    $('input[type=button]').click(function () {
+        $('td').removeAttr('style');
+    });
+
+});
